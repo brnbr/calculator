18a2f619d49d2e89b0d7a639b053eca7400626c7
Inn <dulsfnj413@naver.com>
2026-06-29 20:30:50 +0900

    println -> print

--- src/calculator2/Main.java
+++ src/calculator2/Main.java
@@ -12,18 +12,18 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         while(true){
-            System.out.println("1. 사칙연산,  2. 원의 넓이 : ");
+            System.out.print("1. 사칙연산,  2. 원의 넓이 : ");
             int choice = scan.nextInt();
 
             //사칙연산
             if (choice == 1) {
-                System.out.println("첫 번째 숫자를 입력하세요: ");
+                System.out.print("첫 번째 숫자를 입력하세요: ");
                 int num1 = scan.nextInt();
 
-                System.out.println("두 번째 숫자를 입력하세요: ");
+                System.out.print("두 번째 숫자를 입력하세요: ");
                 int num2 = scan.nextInt();
 
-                System.out.println("사칙연산 기호를 입력하세요: ");
+                System.out.print("사칙연산 기호를 입력하세요: ");
                 char calSym = scan.next().charAt(0);
 
                 if (calSym == '+') {
@@ -49,21 +49,21 @@ public class Main {
             }
 
             //결과 조회
-            System.out.println("저장된 연산 결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+            System.out.print("저장된 연산 결과를 조회하시겠습니까? (inquiry 입력 시 조회): ");
             String inq = scan.next();
             if(inq.equals("inquiry")){
                 acal.inquiryResults();
             }
 
             //맨 앞 데이터 삭제
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            System.out.print("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제): ");
             String rm = scan.next();
             if(rm.equals("remove")){
                 acal.removeResult();
             }
 
             //종료
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료): ");
             String exit = scan.next();
             if (exit.equals("exit")) {
                 return;
