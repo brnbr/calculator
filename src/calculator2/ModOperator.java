a1f3b89fa3ec09d190a3f2728ffedcdefc2ae74e
Inn <dulsfnj413@naver.com>
2026-06-29 14:09:49 +0900

    exception 추가

--- src/calculator2/ModOperator.java
+++ src/calculator2/ModOperator.java
@@ -2,6 +2,10 @@ package calculator2;
 
 public class ModOperator {
     public int operator(int num1, int num2) {
+        if (num2 == 0) {
+            throw new ArithmeticException("0으로 나눌 수 없습니다.");
+        }
+
         return num1 % num2;
     }
 }
