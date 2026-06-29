51529e2be66412ee4dcf3947fc80c1f1c4d7d98d
Inn <dulsfnj413@naver.com>
2026-06-29 14:58:16 +0900

    add exception class

---
+++ src/calculator2/Exception.java
@@ -0,0 +1,9 @@
+package calculator2;
+
+import java.io.IOException;
+
+public class Exception {
+    public void exception() throws IOException {
+        throw new IOException("잘못된 연산자 입니다.");
+    }
+}
