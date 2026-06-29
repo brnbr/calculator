94b235154df7fd4ef59893c8c353cf0426387f0f
Inn <dulsfnj413@naver.com>
2026-06-29 20:15:26 +0900

    toString 추가

--- src/calculator2/Calculator.java
+++ src/calculator2/Calculator.java
@@ -3,7 +3,8 @@ package calculator2;
 import java.util.ArrayList;
 
 public class Calculator {
-    ArrayList<Integer> res = new ArrayList<>();
+    static ArrayList<Integer> res = new ArrayList<>();
+    static ArrayList<Integer> cir = new ArrayList<>();
     int num1;
     int num2;
     int result;
@@ -14,6 +15,10 @@ public class Calculator {
 
     public ArrayList<Integer> getRes() {
         return res;
+    }
+
+    public static ArrayList<Integer> getCir() {
+        return cir;
     }
 
     public int getNum1() {
@@ -35,39 +40,48 @@ public class Calculator {
 
     //연산 결과 조회
     public void inquiryResults() {
-        for (Integer r : res) {
-            System.out.println(r);
+        if (!res.isEmpty()) {
+            System.out.println("사칙연산: " + res);
+        }
+
+        if (!cir.isEmpty()) {
+            System.out.println("원 넓이: " + cir);
         }
     }
 
-    //사칙연산
+    //계산
     //더하기
-    public int add(int num1, int num2) {
-        return 0;
+    public ArrayList<Integer> adder(int num1, int num2) {
+        return res;
     }
 
     //빼기
-    public int sub(int num1, int num2) {
-        return 0;
+    public ArrayList<Integer> sub(int num1, int num2) {
+        return res;
     }
 
     //곱하기
-    public int mul(int num1, int num2) {
-        return 0;
+    public ArrayList<Integer> mul(int num1, int num2) {
+        return res;
     }
 
     //나누기
-    public int div(int num1, int num2) {
-        return 0;
+    public ArrayList<Integer> div(int num1, int num2) {
+        return res;
     }
 
     //나머지
-    public int mod(int num1, int num2) {
-        return 0;
+    public ArrayList<Integer> mod(int num1, int num2) {
+        return res;
     }
 
     //원의 넓이
-    public int calculateCircleArea(int radi) {
-        return 0;
+    public ArrayList<Integer> calculateCircleArea(int radi) {
+        return cir;
+    }
+
+    @Override
+    public String toString() {
+        return "사칙연산: " + res + "\n" + "원 넓이: " + cir;
     }
 }
--- src/calculator2/CircleCalculator.java
+++ src/calculator2/CircleCalculator.java
@@ -7,8 +7,7 @@ public class CircleCalculator extends Calculator{
     @Override
     public ArrayList<Integer> calculateCircleArea(int radi){
         result = (int) (Math.pow(radi,2) * Math.PI);
-        res.add(result);
-
-        return res;
+        cir.add(result);
+        return cir;
     }
 }
