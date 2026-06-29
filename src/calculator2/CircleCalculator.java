6fa36ac97da5db73e5872f08e22f1d6ce1e03b6d
Inn <dulsfnj413@naver.com>
2026-06-29 19:15:50 +0900

    타입 변경

--- src/calculator2/CircleCalculator.java
+++ src/calculator2/CircleCalculator.java
@@ -1,11 +1,14 @@
 package calculator2;
 
+import java.util.ArrayList;
+
 public class CircleCalculator extends Calculator{
 
     @Override
-    public int calculateCircleArea(int radi){
+    public ArrayList<Integer> calculateCircleArea(int radi){
         result = (int) (Math.pow(radi,2) * Math.PI);
+        res.add(result);
 
-        return result;
+        return res;
     }
 }
