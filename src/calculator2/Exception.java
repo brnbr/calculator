ec18b1a385063ab0f7bb951481efc75fbc5ed7b4
Inn <dulsfnj413@naver.com>
2026-06-29 19:16:43 +0900

    출력 문구 수정

--- src/calculator2/Exception.java
+++ src/calculator2/Exception.java
@@ -4,6 +4,6 @@ import java.io.IOException;
 
 public class Exception {
     public void exception() throws IOException {
-        throw new IOException("잘못된 연산자 입니다.");
+        throw new IOException("잘못된 입력 입니다.");
     }
 }
